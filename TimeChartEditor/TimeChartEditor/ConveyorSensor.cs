e38c91a63af3c2caa90eb483d9bc0493d1cc0804
Byeongseon Lee <tjs5007@ajou.ac.kr>
2023-02-18 22:37:08 +0900

    ConveyorSensor.cs 추가

---
+++ TimeChartEditor/TimeChartEditor/ConveyorSensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeChartEditor
+{
+    internal class ConveyorSensor : Module
+    {
+
+
+        public ConveyorSensor()
+        {
+        }
+
+        public override void RunToModule(ActUtlTypeLib.ActUtlTypeClass actUtlType)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                actUtlType.WriteDeviceRandom2("X2", 1,  1);
+                Thread.Sleep(1000);
+                actUtlType.WriteDeviceRandom2("X2", 1,  0);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
