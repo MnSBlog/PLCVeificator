3b543fc13473f303e9f21ec621dc292b146dc611
Simon Anderson <bbubgsang94@ajou.ac.kr>
2023-02-23 17:09:47 +0900

    일단 올리기

---
+++ Pinokio.MPV/Pinokio.MPV/WinFormUtils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YamlDotNet.Serialization;
+
+namespace TimeChartEditor
+{
+    internal class WinFormUtils
+    {
+        public static string OpenFileFinder(string title, string extension)
+        {
+            // extension sample: // "그림 파일 (*.jpg, *.gif, *.bmp) | *.jpg; *.gif; *.bmp; | 모든 파일 (*.*) | *.*";
+            OpenFileDialog Dialog = new OpenFileDialog();
+            Dialog.Title = title;
+            Dialog.Filter = extension; 
+            Dialog.RestoreDirectory = true;
+
+            DialogResult dr = Dialog.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                return Dialog.FileName;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+    internal class FileController
+    {
+        static FileController()
+        {
+            Instance = new FileController();
+        }
+
+        private FileController()
+        {
+
+        }
+
+        public static FileController Instance { get; }
+
+        public void Serialize<T>(string fileName, T value)
+        {
+            var builder = new SerializerBuilder().Build();
+
+            using (var stream = File.Create(fileName))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    builder.Serialize(writer, value);
+                }
+            }
+        }
+
+        public T DeSerialize<T>(string fileName) where T : class
+        {
+            var builder = new DeserializerBuilder().Build();
+
+            using (var stream = File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return builder.Deserialize(reader, typeof(T)) as T;
+                }
+            }
+        }
+    }
+}
---
+++ Pinokio.MPV/SandBox/Testbed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop;
+
+namespace SandBox
+{
+    internal class Testbed
+    {
+        static Excel.Application excelApp = null;
+        static Excel.Workbook workBook = null;
+        static Excel.Worksheet workSheet = null;
+        static void Main(string[] args)
+        {
+
+        }
+        public void ecxelShapeRead()
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);  // 바탕화면 경로
+            string path = Path.Combine(desktopPath, "TimeChart.xlsx");                              // 엑셀 파일 저장 경로
+
+            excelApp = new Excel.Application();                             // 엑셀 어플리케이션 생성
+            workBook = excelApp.Workbooks.Open(path);                       // 워크북 열기
+            workSheet = workBook.Worksheets.get_Item(2) as Excel.Worksheet; // 엑셀 첫번째 워크시트 가져오기
+
+            var image = workSheet.Shapes.Item(3);
+        }
+    }
+}
